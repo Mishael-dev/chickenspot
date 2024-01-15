3edc2124329933f64fcd2a6892af479b8e4d61b7
mishael daap <mishael60567@gmail.com>
2024-01-14 19:59:30 -0800

    refactored the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,12 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { ModeToggle } from "./ThemeButton";
+import { SheetContent, SheetTrigger, Sheet } from "./ui/sheet";
+import { Menu } from "lucide-react";
 
 const navRoutes = [
   {
-    href: "/home",
+    href: "/",
     label: "Home",
   },
   {
@@ -26,16 +28,25 @@ const Header = () => {
       <Container>
         <div className="flex justify-between items-center ">
           <div>
-            <Image
-              height={700}
-              width={700}
-              src={"/images/logo.svg"}
-              alt="chicken on a plate"
-              className="w-52"
-            />
+            <Link href={"/"}>
+              <Image
+                height={700}
+                width={700}
+                src={"/images/logo.svg"}
+                alt="chicken on a plate"
+                className="w-52 shrink-0 hidden md:block"
+              />
+              <Image
+                height={700}
+                width={700}
+                src={"/images/logo icon.svg"}
+                alt="chicken icon"
+                className="w-16 shrink-0 md:hidden"
+              />
+            </Link>
           </div>
 
-          <ul className="flex gap-8">
+          <ul className=" gap-8 hidden md:flex">
             {navRoutes.map((route) => (
               <Button key={route.href} variant={"ghost"}>
                 {" "}
@@ -45,10 +56,27 @@ const Header = () => {
           </ul>
 
           <div className="flex items-center gap-x-4 ">
-            <Button>
-              <Link href={"/menu"}>Order Now</Link>
+            <Button className="shrink-1">
+              <Link href={"/menu"}>Order</Link>
             </Button>
             <ModeToggle />
+            <div className="md:hidden">
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Menu />
+                </SheetTrigger>
+                <SheetContent>
+                  <ul className="flex gap-8 flex-col">
+                    {navRoutes.map((route) => (
+                      <Button key={route.href} variant={"ghost"}>
+                        {" "}
+                        <Link href={route.href}>{route.label}</Link>{" "}
+                      </Button>
+                    ))}
+                  </ul>
+                </SheetContent>
+              </Sheet>
+            </div>
           </div>
         </div>
       </Container>
