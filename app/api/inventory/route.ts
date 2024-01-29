0b899477b647d45d62d43a6303977d99e80c2cd3
Mishael <themishaeldaap@gmail.com>
2024-01-29 18:25:46 -0500

    changed the database url

--- app/api/inventory/route.ts
+++ app/api/inventory/route.ts
@@ -1,7 +1,7 @@
 import prisma from "../../../prisma/connection";
 
 export async function GET() {
-  let inventory = await prisma.inventory.findMany();
+  let data = await prisma.inventory.findMany();
 
-  return Response.json({ inventory });
+  return Response.json({ data });
 }
